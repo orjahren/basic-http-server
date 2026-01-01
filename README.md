981693aa318f080ced525bf8cc5a8044360bec2e
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2026-01-01 10:43:09 +0100

    Opplegg for å bygge med CMake og Ninja.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # basic-http-server
+
 A basic HTTP server, written in C.
+
+## Dependencies
+
+- Cmake
+- Ninja
---
+++ src/main.c
@@ -0,0 +1,7 @@
+// src/main.c
+#include <stdio.h>
+int main(void)
+{
+    printf("Hello, basic HTTP server!\n");
+    return 0;
+}
